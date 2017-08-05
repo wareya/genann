9a411e837c8282b16531fd86e9c8a8545160c715
wareya <wareya@gmail.com>
2017-08-04 20:30:35 -0400

    Personal fork

--- genann.h
+++ genann.h
@@ -27,6 +27,9 @@
 #ifndef __GENANN_H__
 #define __GENANN_H__
 
+#define GENANN_NUMBER_TYPE_IS_FLOAT
+#define GENANN_NUMBER_TYPE float
+
 #include <stdio.h>
 
 #ifdef __cplusplus
@@ -36,44 +39,40 @@ extern "C" {
 #ifndef GENANN_RANDOM
 /* We use the following for uniform random numbers between 0 and 1.
  * If you have a better function, redefine this macro. */
-#define GENANN_RANDOM() (((double)rand())/RAND_MAX)
+#define GENANN_RANDOM() (((GENANN_NUMBER_TYPE)rand())/RAND_MAX)
 #endif
 
 
-typedef double (*genann_actfun)(double a);
+typedef GENANN_NUMBER_TYPE (*genann_actfun)(GENANN_NUMBER_TYPE a);
 
+typedef struct genann_node {
+    GENANN_NUMBER_TYPE output;
+    GENANN_NUMBER_TYPE error; // delta between desired output and real output
+    GENANN_NUMBER_TYPE gradient; // above, in terms of input, not output
+    GENANN_NUMBER_TYPE * weight; // pointer to after self
+    // extended by array of input weights
+} genann_node;
+
+typedef struct genann_nodelist {
+    int inputs; // number of input weights on each node in this layer -- that includes the weight for the hidden/constant input
+    int nodes; // number of nodes
+    genann_actfun act;
+    genann_node ** node; // pointer to after self
+    // extended by list of pointers to nodes
+    // extended by list of nodes
+} genann_nodelist;
 
 typedef struct genann {
-    /* How many inputs, outputs, and hidden neurons. */
-    int inputs, hidden_layers, hidden, outputs;
-
-    /* Which activation function to use for hidden neurons. Default: gennann_act_sigmoid_cached*/
-    genann_actfun activation_hidden;
-
-    /* Which activation function to use for output. Default: gennann_act_sigmoid_cached*/
-    genann_actfun activation_output;
-
-    /* Total number of weights, and size of weights buffer. */
-    int total_weights;
-
-    /* Total number of neurons + inputs and size of output buffer. */
-    int total_neurons;
-
-    /* All weights (total_weights long). */
-    double *weight;
-
-    /* Stores input array and output of each neuron (total_neurons long). */
-    double *output;
-
-    /* Stores delta of each hidden and output neuron (total_neurons - inputs long). */
-    double *delta;
-
+    /* How many inputs, how many layers (including hidden and output but not input) */
+    int inputs, layers;
+    
+    genann_nodelist ** layer; // pointer to after self
+    // extended by list of pointers to layers
+    // extended by list of layers
 } genann;
 
-
-
 /* Creates and returns a new ann. */
-genann *genann_init(int inputs, int hidden_layers, int hidden, int outputs);
+genann * genann_init(int inputs, int layers, int * nodelist, char * functions);
 
 /* Creates ANN from file saved with genann_write. */
 genann *genann_read(FILE *in);
@@ -87,20 +86,23 @@ genann *genann_copy(genann const *ann);
 /* Frees the memory used by an ann. */
 void genann_free(genann *ann);
 
+/* Returns a particular output value from the network */
+GENANN_NUMBER_TYPE genann_output(genann const * ann, int n);
+
 /* Runs the feedforward algorithm to calculate the ann's output. */
-double const *genann_run(genann const *ann, double const *inputs);
+void genann_run(genann const * ann, GENANN_NUMBER_TYPE const * inputs);
 
 /* Does a single backprop update. */
-void genann_train(genann const *ann, double const *inputs, double const *desired_outputs, double learning_rate);
+void genann_train(genann const *ann, GENANN_NUMBER_TYPE const *inputs, GENANN_NUMBER_TYPE const *desired_outputs, GENANN_NUMBER_TYPE learning_rate);
 
 /* Saves the ann. */
 void genann_write(genann const *ann, FILE *out);
 
 
-double genann_act_sigmoid(double a);
-double genann_act_sigmoid_cached(double a);
-double genann_act_threshold(double a);
-double genann_act_linear(double a);
+GENANN_NUMBER_TYPE genann_act_tanh(GENANN_NUMBER_TYPE a);
+GENANN_NUMBER_TYPE genann_act_tanh_cached(GENANN_NUMBER_TYPE a);
+GENANN_NUMBER_TYPE genann_act_threshold(GENANN_NUMBER_TYPE a);
+GENANN_NUMBER_TYPE genann_act_linear(GENANN_NUMBER_TYPE a);
 
 
 #ifdef __cplusplus
